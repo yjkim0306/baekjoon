13b463ee8f4049f0e7f2a574d7b51d896d8cfa4c
김용준 <103955024+yjkim0306@users.noreply.github.com>
2023-02-18 03:31:36 +0900

    Add files via upload

---
+++ #1110.c
@@ -0,0 +1,21 @@
+//더하기 사이클
+
+#include <stdio.h>
+
+int main()
+{
+    int a, count = 0;
+    scanf("%d", &a);
+    int newa = a;
+    while(1)
+    {
+        a = (a/10+a%10)%10+a%10*10, count++;
+        if(a == newa)
+        {
+            printf("%d\n", count);
+            break;
+        }
+    }
+
+    return 0;
+}
