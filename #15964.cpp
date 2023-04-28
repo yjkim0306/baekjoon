af5fb4ef07bc87f744be51bcd5a6c947f8267e9b
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-04-28 15:40:50 +0900

    20230428

---
+++ #15964.cpp
@@ -0,0 +1,16 @@
+// 이상한 기호
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    long long a, b;
+    cin >> a >> b;
+
+    cout << (a + b) * (a - b) << "\n";
+    
+    return 0;
+}
