64667617a6c2c2c318be827228f51106d7fbb794
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-04-30 15:58:22 +0900

    20230430

---
+++ #11942.cpp
@@ -0,0 +1,13 @@
+// 고려대는 사랑입니다
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "고려대학교\n";
+    
+    return 0;
+}
