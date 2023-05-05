a5979f2926ed50e6cda02ca4caa34b815dca045a
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-05-05 23:51:38 +0900

    20230505

--- #2631.cpp
+++ #2631.cpp
@@ -1,11 +1,34 @@
 // 줄세우기
 #include <iostream>
 using namespace std;
+
+int n, m; 
+int arr[201];
+int dp[201];
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
+    cin >> n; 
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for(int i = 0; i < n; i++)
+    {
+        dp[i] = 1;
+        for(int j = 0; j < i; j++)
+        {
+            if(arr[j] < arr[i] && dp[i] < dp[j] + 1)
+                dp[i] = dp[j] + 1;
+            if (m < dp[i])
+                m = dp[i];
+        }
+    }    
+
+    cout << n - m << "\n";
+    
     return 0;
 }
