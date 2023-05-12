305e8cbaf9a0bb2e50a68d90e9e22caafd49b9e9
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-05-12 13:35:14 +0900

    20230512

---
+++ #5337.cpp
@@ -0,0 +1,13 @@
+// 웰컴
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << ".  .   .\n|  | _ | _. _ ._ _  _\n|/\\|(/.|(_.(_)[ | )(/.\n";
+
+    return 0;
+}
