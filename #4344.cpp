9f39a71acabe5e2879b8d95c2e22c21873a44aff
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-06-27 14:27:59 +0900

    재채점

--- #4344.cpp
+++ #4344.cpp
@@ -1,5 +1,6 @@
 // 평균은 넘겠지
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main()
 {
@@ -13,9 +14,9 @@ int main()
     
     while(t--)
     {
-        int arr[1001] = {0,}; // n명의 점수
-        int sum = 0; // 점수의 합
-        double avg = 0.00; // 평균을 저장할 변수
+        double arr[1001] = {0.000,}; // n명의 점수
+        double sum = 0.000; // 점수의 합
+        double avg = 0.000; // 평균을 저장할 변수
 
         cin >> n;
         for(int i = 0; i < n; i++)
@@ -23,16 +24,17 @@ int main()
             cin >> arr[i];
             sum += arr[i];
         }
-        avg = (double)sum / n;
+        avg = sum / (double)n; 
 
         int cnt = 0;
         for(int i = 0; i < n; i++)
         {
             if(arr[i] > avg) cnt++;
         }
-
+        
+        cout << fixed;
         cout.precision(3);
-        cout << (double)cnt / n * 100 << "\n";
+        cout << round((double)cnt / (double)n * 100 * 1000) / 1000 << "%\n";
     }
 
     return 0;
