d5e1439900413ad6c2d96a9005e3816113f689d8
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-07-17 19:45:55 +0900

    20230717

---
+++ #15894.cpp
@@ -0,0 +1,16 @@
+// 수학은 체육과목 입니다
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long n; 
+    cin >> n;
+
+    cout << 4 * n << "\n";
+
+    return 0;
+}
