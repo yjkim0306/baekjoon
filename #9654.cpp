8b9115b0bbf68df0c04246a0d033744dae883e1a
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-07-25 19:59:18 +0900

    20230725

--- #9654.cpp
+++ #9654.cpp
@@ -8,7 +8,7 @@ int main()
     printf("%-15s%-15s%-11s%-10s\n", "N2 Bomber", "Heavy Fighter", "Limited", "21");
     printf("%-15s%-15s%-11s%-10s\n", "J-Type 327", "Light Combat", "Unlimited", "1");
     printf("%-15s%-15s%-11s%-10s\n", "NX Cruiser", "Medium Fighter", "Limited", "18");
-    printf("%-15s%-15s%-11s%-10s\n", "N1 Starfighter", "Medium Fighter", "Unlimited");
+    printf("%-15s%-15s%-11s%-10s\n", "N1 Starfighter", "Medium Fighter", "Unlimited", "25");
     printf("%-15s%-15s%-11s%-10s\n", "Royal Cruiser", "Light Combat", "Limited", "4");
 
     return 0;
