d05c5f4c1e18fc28ab81d8eb17ffdfc352798402
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-07-25 19:59:06 +0900

    20230725

---
+++ #9654.cpp
@@ -0,0 +1,15 @@
+// 나부 함대 데이터
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    printf("%-15s%-15s%-11s%-10s\n", "SHIP NAME", "CLASS", "DEPLOYMENT", "IN SERVICE");
+    printf("%-15s%-15s%-11s%-10s\n", "N2 Bomber", "Heavy Fighter", "Limited", "21");
+    printf("%-15s%-15s%-11s%-10s\n", "J-Type 327", "Light Combat", "Unlimited", "1");
+    printf("%-15s%-15s%-11s%-10s\n", "NX Cruiser", "Medium Fighter", "Limited", "18");
+    printf("%-15s%-15s%-11s%-10s\n", "N1 Starfighter", "Medium Fighter", "Unlimited");
+    printf("%-15s%-15s%-11s%-10s\n", "Royal Cruiser", "Light Combat", "Limited", "4");
+
+    return 0;
+}
