07dbd30cd231e159d9c95c79ca04caed1639fac9
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-09-01 08:16:07 +0900

    20230901

---
+++ #16394.cpp
@@ -0,0 +1,16 @@
+// 홍익대학교
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cout << n - 1946 << "\n";
+    
+    return 0;
+}
