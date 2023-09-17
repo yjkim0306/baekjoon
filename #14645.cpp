2d7893fcac2547d6f643e0893e6bf8bce13056f5
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-09-17 23:05:32 +0900

    20230917

---
+++ #14645.cpp
@@ -0,0 +1,21 @@
+// 와이버스 부릉부릉
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, k;
+    cin >> n >> k;
+    for(int i = 0; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+    }
+
+    cout << "비와이\n";
+    
+    return 0;
+}
