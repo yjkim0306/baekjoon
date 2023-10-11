c18f79971c3f19205c6f77f18878fc2ee4a66d73
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-10-12 04:26:26 +0900

    20231012

---
+++ #27323.cpp
@@ -0,0 +1,16 @@
+// 직사각형
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+    
+    cout << a * b << "\n";
+    
+    return 0;
+}
