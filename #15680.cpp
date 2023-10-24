e3f58e30243b828969c7be227de18892350a0c0f
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-10-24 16:46:10 +0900

    20231024

---
+++ #15680.cpp
@@ -0,0 +1,19 @@
+// 연세대학교
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    if(!n)
+        cout << "YONSEI\n";
+    else
+        cout << "Leading the Way to the Future\n";
+        
+    return 0;
+}
