682f240907c18c8da889b49ced409d0e8f923334
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-11-04 23:52:22 +0900

    20231104

---
+++ #16430.cpp
@@ -0,0 +1,16 @@
+// 제리와 톰
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << b - a << " " << b << "\n";
+    
+    return 0;
+}
