bd28a85e9d988a212d37e482c0306b561c3e2014
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-11-13 02:26:10 +0900

    20231112

---
+++ #2161.cpp
@@ -0,0 +1,28 @@
+// 카드1    
+#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    queue<int> q;
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+        q.push(i);
+
+    while(q.size() != 1)
+    {
+        cout << q.front() << " ";
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+
+    cout << q.front() << "\n";
+    
+    return 0;
+}
