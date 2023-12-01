6f266e43221b40cbe350cfc9c089ed00a1f9c4d6
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-12-02 03:46:52 +0900

    20231201

---
+++ #15439.cpp
@@ -0,0 +1,16 @@
+// 베라의 패션
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cout << n * (n - 1) << "\n";
+    
+    return 0;
+}
