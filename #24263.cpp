7ebc50d3fe2b3361df301ca11662fc1d992918ec
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-12-13 00:34:13 +0900

    20231212

---
+++ #24263.cpp
@@ -0,0 +1,16 @@
+// 알고리즘 수업 - 알고리즘의 수행 시간 2
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cout << n << "\n1\n";
+    
+    return 0;
+}
