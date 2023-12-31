ea1b10e3540a221780494c074727d9d379493118
yjkim0306 <yjkim0306@hanyang.ac.kr>
2023-12-31 21:08:40 +0900

    20231231

---
+++ #15802.cpp
@@ -0,0 +1,16 @@
+// 타노스
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string str;
+    cin >> str;
+
+    cout << "1\n";
+    
+    return 0;
+}
